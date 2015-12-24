1eb61a0893128dcc18a74f5940cf3a9cda22f4a7
Maris Prabha <marisprabha2@gmail.com>
2015-12-24 20:05:41 +0530

    i have learned about if-statement

---
+++ if.py
@@ -0,0 +1,24 @@
+children= 20
+sweets = 23
+balls = 10
+
+if sweets < children:
+	print "some of children don't eat sweets"
+if sweets > children:
+	print "all children eat sweets"
+
+
+if balls < children:
+	print "children play games" 
+if balls > children:
+	print "children play games happy"
+
+balls += 10
+
+if balls <= children:
+	print "balls are greater than or equal to children"
+if balls >= children:
+	print "balls are less than or equal to children"
+if balls == children:
+	print "balls are equal to children"
+
