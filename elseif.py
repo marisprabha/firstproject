67051e5672d1ccd4c3d13ffe3ddc2f41103d7915
Maris Prabha <marisprabha2@gmail.com>
2015-12-25 12:28:22 +0530

    I have learned about elseif statement

---
+++ elseif.py
@@ -0,0 +1,17 @@
+chairs = 10
+students = 12
+desks = 1
+
+if chairs < students:
+	print "remaining students sit on desk"
+elif chairs > students:
+	print "all students sit on chairs"
+else:
+	print "chairs are equal or not equal to students"
+if desks < students:
+	print "some of students sit on chairs"
+elif desks >students:
+	print "all students sit on desks"
+else:
+	print "desks are equal or not equal to students"
+
